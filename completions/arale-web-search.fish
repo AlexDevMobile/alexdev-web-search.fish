c294d2138bcf2971d4eb3807cce737e6c2019ff4
AlerzDev <alex.lemus.dev@gmail.com>
2023-10-05 10:34:33 -0600

    Init project arale-web-search.fish

---
+++ completions/arale-web-search.fish
@@ -0,0 +1,8 @@
+set -l contexts google youtube stackoverflow github
+
+for custom_env in (env | grep ^WEB_SEARCH)
+    set -l context (string split -m 1 = $custom_env)[1]
+    set -a contexts ( echo "$context" | string sub -s 12)
+end
+
+complete -c arale-web-search -f -n __fish_use_subcommand -a "$contexts"
---
+++ funciones/arale-web-search.fish
@@ -0,0 +1,38 @@
+function arale_web_search
+  set -l search_engines \
+  google 'https://www.google.com/search?q=' \
+  stackoverflow 'https://stackoverflow.com/search?q=' \
+  youtube 'https://www.youtube.com/results?search_query=' \
+  github 'https://github.com/search?q=' \
+
+  set -l context_idx (contains -i $argv[1] $search_engines)
+  if not test -n "$context_idx"
+    echo "'$argv[1]' is not supported."
+    return 1
+  end
+
+  set -l url $search_engines[(math $context_idx + 1)]
+
+  if test (count $argv) -ge 2
+    set query (string escape --style=url $argv[2..-1])
+  else
+    set query ''
+  end
+
+  set -l open_cmd
+
+  switch (uname)
+    case Linux
+      set open_cmd "nohup xdg-open"
+    case Darwin
+      set open_cmd "open"
+    end
+
+  eval "$open_cmd '$url$query' &> /dev/null & disown"
+
+end
+
+alias google='arale_web_search google'
+alias stackoverflow='arale_web_search stackoverflow'
+alias youtube='arale_web_search youtube'
+alias github='arale_web_search github'
